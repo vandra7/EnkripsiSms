ac8fcab4f7832209717c9c7dd76eca40855d1424
alfa_khrisna <alfa.khrisna@student.vinci.be>
2019-10-22 17:59:30 +0200

    Impl: TransEncrypt partial dev
    Signed-off-by: alfa_khrisna <alfa.khrisna@student.vinci.be>

--- app/src/main/java/com/example/enkripsisms/TransEncrypt.java
+++ app/src/main/java/com/example/enkripsisms/TransEncrypt.java
@@ -3,16 +3,38 @@ package com.example.enkripsisms;
 public final class TransEncrypt {
 
     /**
+     * @author Alfa Khrisna
      * Encrypts a message with a double transposition cypher
      * @param msg
      * @param keyword
      * @return the encrypted message
      */
     public static String encrypt(String msg, String keyword) {
+        // Get cypher length
+        int c_length = keyword.length();
+
+        // Get cypher order
+        keyword = keyword.toUpperCase();
+        int[] p_tab = new int[c_length];
+        for(int i = 0; i < c_length; i++) {
+            p_tab[i] = (int)keyword.charAt(i);
+        }
+
+        // Construction of string
+        StringBuilder sb = new StringBuilder();
+        char[] c_word = new char[c_length];
+        msg = msg.replaceAll(" ", "");
+        msg.toUpperCase();
+        int pointer = 0;
+        int min = 0;
+        // TODO
+
+        // Dummy return
         return null;
     }
 
     /**
+     * @author Alfa Khrisna
      * Decrypts a double transposition cypher
      * @param msg
      * @param keyword
